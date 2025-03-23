e8bba5109ccc9ab990603115c7058fdb0f7b723f
Samrat Vashisht <140803892+SxmrxtV@users.noreply.github.com>
2025-03-23 19:37:05 +0530

    Update Prototype 1.0 .py

--- Prototype 1.0 .py
+++ Prototype 1.0 .py
@@ -1,7 +1,7 @@
-#Importing stuff
+#Importing Modules
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider as SL
+from matplotlib.widgets import Slider as sl
 
 #We're gonna try and plot a relatively easy graph representing the equation x = a*sin(wt+phi)
 
@@ -9,29 +9,28 @@ def wave(a=1, w=2*np.pi, phi=0):
     t = np.linspace(0,2*np.pi, 1000)
     x = a*np.sin(w*t + phi)
     ax.clear()
-    ax.plot(t, x, label=r'$x = a \sin(\omega t + \phi)$', color='b')
+    ax.plot(t, x)
     ax.axhline(0, color='black', linewidth=0.5)
     ax.axvline(0, color='black', linewidth=0.5)
     ax.set_xlabel('Time (t)')
     ax.set_ylabel('Displacement (x)')
-    ax.set_title("One-Dimensional Wave Equation")
-    ax.legend()
     ax.grid()
     fig.canvas.draw_idle()
 
-fig, ax=plt.subplots(figsize=(8,4))
+fig, ax = plt.subplots(figsize=(8, 4))
 plt.subplots_adjust(bottom=0.5)
 wave()
 
-#Sliders
+
+#Adding the sliders
 
 ax_a = plt.axes([0.2, 0.15, 0.65, 0.03])
 ax_w = plt.axes([0.2, 0.1, 0.65, 0.03])
 ax_phi = plt.axes([0.2, 0.05, 0.65, 0.03])
 
-slider_a = SL(ax_a, 'Amplitude (a)', 0.1, 2.0, valinit=1)
-slider_w = SL(ax_w, "Frequency (w)", 0.1, 10.0, valinit=2*np.pi)
-slider_phi = SL(ax_phi, 'Phase Difference (phi)', -np.pi, np.pi, valinit=0)
+slider_a = sl(ax_a, 'Amplitude', 0.1, 10, valinit=1)
+slider_w = sl(ax_w, 'Frequency', 0.1, 20*np.pi, valinit=2*np.pi)
+slider_phi = sl(ax_phi, 'Phase Difference', -2*np.pi, 2*np.pi, valinit=0)
 
 def update(val):
     wave(slider_a.val, slider_w.val, slider_phi.val)
